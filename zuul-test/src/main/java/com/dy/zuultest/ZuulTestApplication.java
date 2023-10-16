1212138b52dbb2822dc02e1da8442715f23b9052
公司 <719893332@qq.com>
2023-10-16 17:47:36 +0800

    网关

---
+++ zuul-test/src/main/java/com/dy/zuultest/ZuulTestApplication.java
@@ -0,0 +1,13 @@
+package com.dy.zuultest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulTestApplication.class, args);
+    }
+
+}
